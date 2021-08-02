34318c55c3b529aa3ac013416f488e5e4777de2f
BrianKhaifah <briankhaifah@gmail.com>
2021-08-02 13:32:43 +0300

    added atest anotation on ceaser.test

--- src/CeaserCipherTest.java
+++ src/CeaserCipherTest.java
@@ -3,4 +3,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CeaserCipherTest {
-}
+
+        @Test
+        public void tests() {
+
+            assertTrue("Ceaser", true);
+        }
+    }
+
