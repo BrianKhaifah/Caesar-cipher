2418612cb12472117ba99b2d4f2d5a23536af8ea
BrianKhaifah <briankhaifah@gmail.com>
2021-08-02 13:44:12 +0300

    Added a Cipher Strin on  PSVM function

--- src/CeaserCipher.java
+++ src/CeaserCipher.java
@@ -4,6 +4,12 @@ public class CeaserCipher {
 
     public static void main(String[] args) {
 
+        String cipher = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        String  lowerCase  =  cipher.toLowerCase();
+
+
+
 
     }
 
