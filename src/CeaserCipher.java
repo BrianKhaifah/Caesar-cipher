c4f2a981d7fa98b9b386939c70617d707827f4a3
BrianKhaifah <briankhaifah@gmail.com>
2021-08-02 13:54:19 +0300

    Added a Cipher String variable on  PSVM function

--- src/CeaserCipher.java
+++ src/CeaserCipher.java
@@ -1,4 +1,5 @@
 import java.until.Scanner;
+import java.util.Scanner;
 
 public class CeaserCipher {
 
@@ -8,6 +9,11 @@ public class CeaserCipher {
 
         String  lowerCase  =  cipher.toLowerCase();
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Your Text;...");
+        String message = scan.next();
+        int length = message.length();
+        System.out.println( " Enter the Key;...");
 
 
 
